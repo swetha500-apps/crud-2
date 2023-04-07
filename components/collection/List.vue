94967209e06a2d7dd9926cf1ade71ffac71bc000
swetha500-apps <swetha.sankati@500apps.com>
2023-04-07 11:36:32 +0530

    changes

--- components/collection/List.vue
+++ components/collection/List.vue
@@ -2,11 +2,21 @@
   <div class="px-4 sm:px-6 lg:px-8">
     <div class="sm:flex sm:items-center">
       <div class="sm:flex-auto">
-        <h1 class="text-base font-semibold leading-6 text-gray-900">Transactions</h1>
-        <p class="mt-2 text-sm text-gray-700">A table of placeholder stock market data that does not make any sense.</p>
+        <h1 class="text-base font-semibold leading-6 text-gray-900">
+          Transactions
+        </h1>
+        <p class="mt-2 text-sm text-gray-700">
+          A table of placeholder stock market data that does not make any sense.
+        </p>
       </div>
       <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"  @click="openAddsidebar">Projects</button>
+        <button
+          type="button"
+          class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          @click="openAddsidebar"
+        >
+          Projects
+        </button>
       </div>
     </div>
     <div class="mt-8 flow-root">
@@ -14,55 +24,104 @@
         <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
           <table class="min-w-full divide-y divide-gray-300">
             <thead>
-              <tr >
-                <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Name</th>
-                <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">ListName</th>
-                <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Details</th>
-                <th scope="col" class="whitespace-nowrap  py-3.5 text-left text-sm font-semibold text-gray-900">Specifications</th>
-                <th scope="col" class="whitespace-nowrap  py-3.5 text-left text-sm font-semibold text-gray-900">ProjectAge</th>
-                <th scope="col" class="whitespace-nowrap py-3.5 text-left text-sm font-semibold text-gray-900">Logo</th>
-                <th scope="col" class="whitespace-nowrap  py-3.5 text-left text-sm font-semibold text-gray-900">DefaultImage</th>
-                <th scope="col" class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-0">
+              <tr>
+                <th
+                  scope="col"
+                  class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  ListName
+                </th>
+                <th
+                  scope="col"
+                  class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Details
+                </th>
+                <th
+                  scope="col"
+                  class="whitespace-nowrap py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Specifications
+                </th>
+                <th
+                  scope="col"
+                  class="whitespace-nowrap py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  ProjectAge
+                </th>
+                <th
+                  scope="col"
+                  class="whitespace-nowrap py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Logo
+                </th>
+                <th
+                  scope="col"
+                  class="whitespace-nowrap py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  DefaultImage
+                </th>
+                <th
+                  scope="col"
+                  class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-0"
+                >
                   <span class="sr-only">Edit</span>
                 </th>
-                <th scope="col" class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-0">
+                <th
+                  scope="col"
+                  class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-0"
+                >
                   <span class="sr-only">Delete</span>
                 </th>
               </tr>
             </thead>
             <tbody class="divide-y divide-gray-200 bg-white">
               <tr v-for="project in props.projectData" :key="project.name">
-                <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">{{ project.name }}</td>
-                <td class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">{{ project.listing_type_name }}</td>
-                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">{{ project.details }}</td>
-                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{ project.specifications }}</td>
-                <td class="whitespace-nowrap  py-2 text-sm text-gray-500">{{ project.age_of_the_project }}</td>
-                <td class="whitespace-nowrap  py-2 text-sm text-gray-500">{{ project.logo_url }}</td>
-                <td class="whitespace-nowrap  py-2 text-sm text-gray-500">{{ project.default_image_url }}</td>
-               
+                <td
+                  class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0"
+                >
+                  {{ project.name }}
+                </td>
+                <td
+                  class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900"
+                >
+                  {{ project.listing_type_name }}
+                </td>
+                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
+                  {{ project.details }}
+                </td>
+                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                  {{ project.specifications }}
+                </td>
+                <td class="whitespace-nowrap py-2 text-sm text-gray-500">
+                  {{ project.age_of_the_project }}
+                </td>
+                <td class="whitespace-nowrap py-2 text-sm text-gray-500">
+                  {{ project.logo_url }}
+                </td>
+                <td class="whitespace-nowrap py-2 text-sm text-gray-500">
+                  {{ project.default_image_url }}
+                </td>
+
                 <div class="flex">
+                  <PencilSquareIcon
+                    @click="openEditsidebar(project)"
+                    class="h-5 w-5 mr-3"
+                    aria-hidden="true"
+                  ></PencilSquareIcon>
 
-<PencilSquareIcon
-
-@click="openEditsidebar"
-
-class="h-5 w-5 mr-3"
-
-aria-hidden="true"
-
-></PencilSquareIcon>
-
-<TrashIcon
-
-@click="deleteTemplate()"
-
-class="h-5 w-5"
-
-aria-hidden="true"
-
-></TrashIcon>
-
-</div>
+                  <TrashIcon
+                    @click="deleteProject(project.name)"
+                    class="h-5 w-5"
+                    aria-hidden="true"
+                  ></TrashIcon>
+                </div>
               </tr>
             </tbody>
           </table>
@@ -72,25 +131,22 @@ aria-hidden="true"
   </div>
 </template>
 <script setup>
-import {ref,defineEmits,defineProps} from "vue"
-import {
-  PencilSquareIcon,
-  TrashIcon,
-} from "@heroicons/vue/24/outline";
-const props=defineProps({
-    projectData:{
-        type:Object
-    }
-})
-const emit=defineEmits(["openAddSidebar","openEditSidebar"])
-const openAddsidebar=()=>{
- emit("openAddSidebar")
-}
-const openEditsidebar=()=>{
- emit("openEditSidebar",object._rawValue[0])
+import { ref, defineEmits, defineProps } from "vue";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/vue/24/outline";
+const props = defineProps({
+  projectData: {
+    type: Object,
+  },
+});
+const emit = defineEmits(["openAddSidebar", "openEditSidebar","deleteProject"]);
+const openAddsidebar = () => {
+  emit("openAddSidebar");
+};
+const openEditsidebar = (value) => {
+  emit("openEditSidebar", value);
+};
+const deleteProject=(name)=>{
+ emit("deleteNotes",name)
 
 }
-const object=ref([])
-object.value.push(props.projectData)
-console.log("projectData-->",object._rawValue[0])
 </script>
--- components/collection/Main.vue
+++ components/collection/Main.vue
@@ -1,48 +1,64 @@
 <template>
-<CollectionList @openAddSidebar="openAddSidebar" @openEditSidebar="openEditSidebar" :projectData="projects" :key="render"/>
- <div v-if="addSidebar" :key="Addrender">
-    <CollectionAdd @postData="postData"/>
+  <CollectionList
+    @openAddSidebar="openAddSidebar"
+    @openEditSidebar="openEditSidebar"
+     @deleteProject="deleteProject"
+    :projectData="projects"
+    :key="render"
+  />
+  <div v-if="addSidebar" :key="Addrender">
+    <CollectionAdd @postData="postData" />
   </div>
-   <div v-if="editSidebar" :key="Editrender">
-    <CollectionEdit :name="name" :listName="listName" :details="details" :specifications="specifications" :projectAge="projectAge" :logo="logo" :defaultImage="defaultImage"/>
+  <div v-if="editSidebar" :key="Editrender">
+    <CollectionEdit
+      :name="name"
+      :listName="listName"
+      :details="details"
+      :specifications="specifications"
+      :projectAge="projectAge"
+      :logo="logo"
+      :defaultImage="defaultImage"
+    />
   </div>
 </template>
 <script setup lang="ts">
-import {ref,onMounted} from "vue";
-const render=ref(0)
-const addSidebar=ref(false)
-const editSidebar=ref(false)
-const Addrender=ref(0)
-const Editrender=ref(0)
-const projects=ref("")
-const name=ref("")
-const listName=ref("")
-const details=ref("")
-const specifications=ref("")
-const projectAge=ref("")
-const logo=ref("")
-const defaultImage=ref("")
-        const getData= useAuthLazyFetch(
-  "https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/?offset=0&limit=100&sort_column=id&sort_direction=desc",""
+import { ref, onMounted } from "vue";
+const render = ref(0);
+const addSidebar = ref(false);
+const editSidebar = ref(false);
+const Addrender = ref(0);
+const Editrender = ref(0);
+const projects = ref("");
+const name = ref("");
+const listName = ref("");
+const details = ref("");
+const specifications = ref("");
+const projectAge = ref("");
+const logo = ref("");
+const defaultImage = ref("");
+const getData = useAuthLazyFetch(
+  "https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/?offset=0&limit=100&sort_column=id&sort_direction=desc",
+  ""
 );
 projects.value = getData.data._rawValue;
-const openAddSidebar=()=>{
-    Addrender.value++
-    addSidebar.value=true
-}
-const openEditSidebar=(data:any)=>{
-    name.value=data[0].name
-    listName.value=data[0].listing_type_name
-    details.value=data[0].details
-    specifications.value=data[0].specifications
-    projectAge.value=data[0].age_of_the_project
-    logo.value=data[0].logo_url
-    defaultImage.value=data[0].default_image_url
-    Editrender.value++
-    editSidebar.value=true
-}
-async function postData(value:any) {
-   
+const openAddSidebar = () => {
+  Addrender.value++;
+  addSidebar.value = true;
+};
+const individualData=ref("")
+const openEditSidebar = (data: any) => {
+  individualData.value=data
+  name.value = data.name;
+  listName.value = data.listing_type_name;
+  details.value = data.details;
+  specifications.value = data.specifications;
+  projectAge.value = data.age_of_the_project;
+  logo.value = data.logo_url;
+  defaultImage.value = data.default_image_url;
+  Editrender.value++;
+  editSidebar.value = true;
+};
+ const postData=(value: any)=> {
   const postoptions = {
     method: "POST",
     headers: {
@@ -50,29 +66,77 @@ async function postData(value:any) {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoiN2ZiMzBkNzhmM2NmNGEwZmJiZWNkZjJkOGM2ZjNhMGEiLCJkIjoiMTY4MDA3NyIsInIiOiJzYSIsInAiOiJmcmVlIiwiYSI6ImZpbmRlci5pbyIsImwiOiJ1czEiLCJleHAiOjE2ODMyNzM0NzR9.QjMEQKeWqKdjLekJkiFGTdhJ3iwilHM5Aa9FEqbWvOI`,
     },
     body: {
-     name: value.name,
-  listing_type_name: value.listName,
-  category: "Residential",
-  sub_category: "Apartment",
-  status: "Fully Constructed",
-  details: value.details,
-  specifications: value.specifications,
-  possession_date: "2023-04-06",
-  age_of_the_project: value.projectAge,
-  logo_url: value.logo,
-  total_project_area: 0,
-  metric: "sq.ft",
-  default_image_url: value.defaultImage,
-  visit_count: 0,
-  rera_approved: true,
-  approve_status: "Active"
-
+      name: value.name,
+      listing_type_name: value.listName,
+      category: "Residential",
+      sub_category: "Apartment",
+      status: "Fully Constructed",
+      details: value.details,
+      specifications: value.specifications,
+      possession_date: "2023-04-06",
+      age_of_the_project: value.projectAge,
+      logo_url: value.logo,
+      total_project_area: 0,
+      metric: "sq.ft",
+      default_image_url: value.defaultImage,
+      visit_count: 0,
+      rera_approved: true,
+      approve_status: "Active",
     },
   };
-  render.value++
-  const data =  useAuthLazyFetchPost(
+  render.value++;
+  const data = useAuthLazyFetchPost(
     "https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/",
     postoptions
   );
+}
+const  deleteProject=(id:any)=>{
+    const deleteOptions = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoiN2ZiMzBkNzhmM2NmNGEwZmJiZWNkZjJkOGM2ZjNhMGEiLCJkIjoiMTY4MDA3NyIsInIiOiJzYSIsInAiOiJmcmVlIiwiYSI6ImZpbmRlci5pbyIsImwiOiJ1czEiLCJleHAiOjE2ODMyNzM0NzR9.QjMEQKeWqKdjLekJkiFGTdhJ3iwilHM5Aa9FEqbWvOI`,
+    },
+}
+ const deleteData = useAuthLazyFetchDelete(
+    `https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/${id}`,
+    deleteOptions
+  );
+projects.value.forEach((item:any,index:any)=>{
+    (item.uid==id)?
+    projects.value.splice(index, 1):""
+      }) 
+}
+const putData=(individualData:any)=> {
+let body={
+   name: individualData.value.name,
+      listing_type_name: individualData.listName,
+      category: "Residential",
+      sub_category: "Apartment",
+      status: "Fully Constructed",
+      details: individualData.value.details,
+      specifications: individualData.value.specifications,
+      possession_date: "2023-04-06",
+      age_of_the_project: individualData.value.projectAge,
+      logo_url: individualData.value.logo,
+      total_project_area: 0,
+      metric: "sq.ft",
+      default_image_url: individualData.value.defaultImage,
+      visit_count: 0,
+      rera_approved: true,
+      approve_status: "Active",
+}
+const { data: items, pending } =    useAuthLazyFetchPut( `https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/${individualData.value.uid}`,{body: JSON.stringify(body)})
+projects.value.forEach((item:any,index:any)=>{
+    if(item.uid==individualData.value.uid){
+    item.name=individualData.value.name
+    item.listName=individualData.value.listing_type_name
+    item.details=individualData.value.details
+    item.specifications=individualData.value.specifications
+    individualData.value.age_of_the_project
+    individualData.value.default_image_url
+    individualData.value.logo_url
+      }})
 }
 </script>
