a13052c17c5373d80c3f8c6bb6411cd569a0cec7
swetha500-apps <swetha.sankati@500apps.com>
2023-04-08 16:26:44 +0530

    final changes

---
+++ components/collection/Edit.vue
@@ -0,0 +1,98 @@
+<template>
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <div class="fixed inset-0" />
+      <div class="fixed inset-0 overflow-hidden">
+        <div class="absolute inset-0 overflow-hidden">
+          <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
+              <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                  <div class="bg-indigo-700 px-4 py-6 sm:px-6">
+                    <div class="flex items-center justify-between">
+                      <DialogTitle class="text-base font-semibold leading-6 text-white">Add Custom Field</DialogTitle>
+                      <div class="ml-3 flex h-7 items-center">
+                        <button type="button" class="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white" @click="open = false">
+                          <span class="sr-only">Close panel</span>
+                          <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </div>
+                    <div class="mt-1">
+                      <p class="text-sm text-indigo-300">Set a label,select custom field type and edit</p>
+                    </div>
+                  </div>
+                  <form>
+                  <div class="relative flex-1 px-4 py-6 sm:px-6">
+                 
+                    <!-- input feild for label -->
+    <label for="label" class="block text-sm font-medium leading-6 text-gray-900">Label</label>
+    <div class="mt-2">
+      <input v-model="editPrefilledData.name" type="text"  id="label" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="Label" />
+    </div>
+                     <!-- input feild for description -->
+    <label for="description" class="block text-sm font-medium leading-6 text-gray-900 mt-2">Description</label>
+    <div class="mt-2">
+      <input v-model="editPrefilledData.type_data.description" type="text"  id="description" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="Description" />
+    </div>
+      <!-- input feild for type -->
+     <label for="type" class="block text-sm font-medium leading-6 text-gray-900 mt-2">Type</label>
+ <select
+                            v-model="editPrefilledData.type"
+                            class="bg-white block mb-3 px-3 rounded-md border-0 py-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6 w-[100%]"
+                           placeholder="Please select type"
+                          >
+                            <option value="TEXTBOX">TEXTBOX</option>
+                            <option value="RADIO_BUTTONS">RADIOBUTTONS</option>
+                            <option value="MULTI_CHECKBOX">
+                              MULTICHECKBOX
+                            </option>
+                          
+                          </select>
+      <!-- input feild for placeholder --> 
+     <label for="placeholder" class="block text-sm font-medium leading-6 text-gray-900 mt-2">Placeholder</label>
+    <div class="mt-2">
+      <input v-model="editPrefilledData.type_data.placeholder" type="text"  id="placeholder" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="placeholder" />
+    </div>
+    <!-- toggle button for mandatory -->
+  <SwitchGroup as="div" class="flex items-center mt-3">
+    <Switch v-model="editPrefilledData.type_data.is_required" :class="[editPrefilledData.type_data.is_required ? 'bg-indigo-600' : 'bg-gray-200', 'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2']">
+      <span aria-hidden="true" :class="[editPrefilledData.type_data.is_required ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out']" />
+    </Switch>
+    <SwitchLabel as="span" class="ml-3 text-sm">
+      <span class="font-medium text-gray-900">Mandatory</span>
+    </SwitchLabel>
+  </SwitchGroup>
+  <div class="flex flex-row float-right mt-5">
+     <button type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mr-3" @click="open=false">Cancel</button>
+ <button type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="saved">Save</button>
+  </div>
+                  </div>
+                  </form>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+import { ref,defineEmits,defineProps } from 'vue'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot,Switch,SwitchGroup, SwitchLabel,Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+// Define emits
+const emit=defineEmits(["saved"])
+const props=defineProps({
+    editPrefilledData:Object
+})
+const open = ref(true)
+const saved=()=>{
+ emit("saved",props.editPrefilledData)
+ open.value=false
+}
+
+</script>
--- components/collection/Main.vue
+++ components/collection/Main.vue
@@ -1,6 +1,7 @@
 <template>
-<CollectionList @add="add" :customFields="customFields"/>
-<CollectionAdd  v-if="isAdd" :key="render" @save="saveCustomField"/>
+<CollectionList @add="add" @edit="edit" @delete="deletefield" :customFields="customFields"/>
+<CollectionAdd  v-if="isAdd" :key="addRender" @save="addCustomField"/>
+<CollectionEdit v-if="isEdit" :key="editRender" :editPrefilledData="editPrefilledData" @saved="saved"/>
 </template>
 <script lang="ts" setup>
 import { ref,defineProps} from 'vue'
@@ -8,22 +9,12 @@ import { ref,defineProps} from 'vue'
 const props = defineProps({
   url: {
     type: String,
-    default: "https://v1-orm-lib.mars.hipso.cc/api/custom-fields/", 
-         
-  },
-  isActive: {
-    type: String,
-    default: "1",
-  },
-  projectId: {
-    type: String,
-    default: "95",
+    default: "https://v1-orm-lib.mars.hipso.cc/api/custom-fields/",   
   },
   // Email customFields for contacts, deal etc.
   entity: {
     type: String,
      default: "CONTACTS",
-    
   },
 });
 // Authorization token
@@ -32,12 +23,22 @@ const authHeader = {
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoiN2ZiMzBkNzhmM2NmNGEwZmJiZWNkZjJkOGM2ZjNhMGEiLCJkIjoiMTY4MDA3NyIsInIiOiJzYSIsInAiOiJmcmVlIiwiYSI6ImZpbmRlci5pbyIsImwiOiJ1czEiLCJleHAiOjE2ODMyNzM0NzR9.QjMEQKeWqKdjLekJkiFGTdhJ3iwilHM5Aa9FEqbWvOI",
 };
 const isAdd=ref(false)
-const render=ref(0)
+const isEdit=ref(false)
+const addRender=ref(0)
+const editRender=ref(0)
 const getData=ref();
+//open add sidebar
 const add = () => {  
   isAdd.value = true;
-  render.value++
+  addRender.value++
 };
+const editPrefilledData=ref("")
+//open edit sidebar
+const edit = (editData:any) => {  
+  isEdit.value = true;
+  editPrefilledData.value=editData
+     editRender.value++
+}
 // Get customFields data using api call
 const { data: customFields } = await useLazyFetch(
   `${props.url}${props.entity}?offset=0&limit=100&sort_column=id&sort_direction=desc`,
@@ -47,9 +48,8 @@ const { data: customFields } = await useLazyFetch(
   }
 );
 getData.value=customFields.value
-console.log("getData.value--->",getData.value)
-// Save customField data
-const saveCustomField = async (postBody: any) => {
+// Add customField data
+const addCustomField = async (postBody: any) => {
   // Post call hits when click on save button
   const { data: response } = await useLazyFetch(props.url, {
     method: "POST",
@@ -58,33 +58,22 @@ const saveCustomField = async (postBody: any) => {
   });
   getData.value.unshift(response.value);
 };
-// Update templates data
-// const edit = async (template: any) => {
-//   const postData = ref({
-//     project_id: props.projectId,
-//     name: template.name,
-//     subject: template.subject,
-//     body: template.body,
-//     is_active: props.isActive,
-//     type: template.type,
-//     share_type: props.shareType,
-//     category: props.entity,
-//   });
-//   // Put call hits when click on save button
-//   const { data: response } = await useLazyFetch(`${props.url}${template.uid}`, {
-//     method: "PUT",
-//     headers: authHeader,
-//     body: postData.value,
-//   });
-//   if (response) {
-//     templates.value.forEach((templateData: any) => {
-//       if (currentTemplate.value.uid === templateData.uid) {
-//         templateData.name = template.name;
-//         templateData.subject = template.subject;
-//         templateData.body = template.body;
-//         templateData.type = template.type;
-//       }
-//     });
-//   }
-// };
+//Edit custom field
+const saved=async(PutBody:any)=>{
+    // Put call hits when click on save button
+  const { data: response } = await useLazyFetch(`${props.url}${PutBody.uid}`, {
+    method: "PUT",
+    headers: authHeader,
+    body: PutBody,
+  });
+};
+
+// Delete customField based on uid
+const deletefield = (uid: string,fieldIndex:any) => {
+  const { data: response } = useLazyFetch(`${props.url}${uid}`, {
+    method: "DELETE",
+    headers: authHeader,
+  });
+   getData.value.splice(fieldIndex, 1);
+};
 </script>
