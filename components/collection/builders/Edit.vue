1eed5b5a9f8ce07b9f54500febbd3bbb42aef9f2
swetha500-apps <swetha.sankati@500apps.com>
2023-04-10 14:02:01 +0530

    add employee

--- components/collection/builders/Edit.vue
+++ components/collection/builders/Edit.vue
@@ -1,100 +1,100 @@
 <template>
   <TransitionRoot as="template" :show="open">
     <Dialog as="div" class="relative z-10" @close="open = false">
-      <div class="fixed inset-0" />
-      <div class="fixed inset-0 overflow-hidden">
-        <div class="absolute inset-0 overflow-hidden">
-          <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-            <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
-              <DialogPanel class="pointer-events-auto w-screen max-w-md">
-                <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                  <div class="bg-indigo-700 px-4 py-6 sm:px-6">
-                    <div class="flex items-center justify-between">
-                      <DialogTitle class="text-base font-semibold leading-6 text-white">Add Builder</DialogTitle>
-                      <div class="ml-3 flex h-7 items-center">
-                        <button type="button" class="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white" @click="open = false">
-                          <span class="sr-only">Close panel</span>
-                          <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-                        </button>
-                      </div>
-                    </div>
-                    <div class="mt-1">
-                      <p class="text-sm text-indigo-300"></p>
-                    </div>
-                  </div>
-                  <form>
-                  <div class="relative flex-1 px-4 py-6 sm:px-6">        
-                    <!-- input feild for name -->
-    <label for="name" class="block text-sm font-medium leading-6 text-gray-900">NAME</label>
-    <div class="mt-2">
-      <input v-model="editPrefilledData.name" type="text"  id="name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="NAME" />
-    </div>
-                     <!-- input feild for email -->
-    <label for="email" class="block text-sm font-medium leading-6 text-gray-900 mt-2">EMAIL</label>
-    <div class="mt-2">
-      <input v-model="editPrefilledData.email" type="text"  id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="EMAIL" />
-    </div>
-      <!-- input feild for phone_number -->
-     <label for="phone_number" class="block text-sm font-medium leading-6 text-gray-900 mt-2">PHONE NUMBER</label>
-    <div class="mt-2">
-      <input v-model="editPrefilledData.phone_number" type="text"  id="phone_number" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="PHONE NUMBER" />
-    </div>
-      <!-- input feild for established_year --> 
-     <label for="established_year" class="block text-sm font-medium leading-6 text-gray-900 mt-2">ESTABLISHED YEAR</label>
-    <div class="mt-2">
-      <input v-model="editPrefilledData.established_year" type="text"  id="established_year" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="ESTABLISHED YEAR" />
-    </div>
-     <!-- input feild for logo --> 
-     <label for="logo" class="block text-sm font-medium leading-6 text-gray-900 mt-2">LOGO</label>
-    <div class="mt-2">
-      <input v-model="editPrefilledData.logo" type="text"  id="logo" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="LOGO" />
-    </div>
-     <!-- input feild for tag_line --> 
-     <label for="tag_line" class="block text-sm font-medium leading-6 text-gray-900 mt-2">TAG LINE</label>
-    <div class="mt-2">
-      <input v-model="editPrefilledData.tag_line" type="text"  id="tag_line" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="TAG LINE" />
-    </div>
-  
-    <!-- toggle button for active -->
-  <SwitchGroup as="div" class="flex items-center mt-3">
-    <Switch v-model="editPrefilledData.is_active" :class="[editPrefilledData.is_active ? 'bg-indigo-600' : 'bg-gray-200', 'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2']">
-      <span aria-hidden="true" :class="[editPrefilledData.is_active ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out']" />
-    </Switch>
-    <SwitchLabel as="span" class="ml-3 text-sm">
-      <span class="font-medium text-gray-900">Active</span>
-    </SwitchLabel>
-  </SwitchGroup>
-  <!-- buttons for save and cancel -->
-  <div class="flex flex-row float-right mt-5">
-     <button type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mr-3" @click="open=false">Cancel</button>
- <button type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="saved">Save</button>
-  </div>
-                  </div>
-                  </form>
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <div>
+                <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                  <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
                 </div>
-              </DialogPanel>
-            </TransitionChild>
-          </div>
+                <div class="mt-3 text-center sm:mt-5">
+                  <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">EDIT EMPLOYEE</DialogTitle>
+                 <div class="mt-2">
+                                <!-- input feild for name --> 
+    <div class="flex flex-row mb-3">
+       <label for="name" class="block text-sm font-medium leading-6 text-gray-900  mr-5 mt-3 ">NAME</label>
+    <div class="mt-2">
+      <input v-model="editPrefilledData.name" type="text"  id="name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="name" />
+    </div>
+    </div>
+                           <!-- input feild for age --> 
+    <div class="flex flex-row mb-3">
+       <label for="age" class="block text-sm font-medium leading-6 text-gray-900 mt-3 mr-5 ml-3">AGE</label>
+    <div class="mt-2">
+      <input v-model="editPrefilledData.age" type="text"  id="age" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="age" />
+    </div>
+    </div>
+       <!-- input feild for gender -->
+          <div class="flex flex-row mb-3">
+     <label for="type" class=" block text-sm font-medium leading-6 text-gray-900 mt-3 ml-3">GENDER</label>
+ <select
+                            v-model="editPrefilledData.gender"
+                            class="bg-white block mb-3 px-3 rounded-md border-0 py-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6 ml-3"
+                         
+                          >
+                            <option value='' selected  >Please select a type</option>
+                            <option value="MALE" >MALE</option>
+                            <option value="FEMALE">FEMALE</option>
+                          
+                          
+                          </select>
+                          </div>
+                           <!-- input feild for date of joining --> 
+    <div class="flex flex-row mb-3">
+       <label for="doj" class="block text-sm font-medium leading-6 text-gray-900 mt-2 mr-5 ml-3">DATE OF JOINING</label>
+    <div class="mt-2">
+      <input v-model="editPrefilledData.doj" type="text"  id="doj" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3" placeholder="date of joining" />
+    </div>
+    </div>
+      <!-- input feild for designation -->
+      <div class="flex flex-row mb-3">
+     <label for="type" class=" block text-sm font-medium leading-6 text-gray-900 mt-3 ml-3">DESIGNATION</label>
+ <select
+                            v-model="editPrefilledData.designation"
+                            class="bg-white block mb-3 px-3 rounded-md border-0 py-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-300 sm:text-sm sm:leading-6 ml-3"
+                         
+                          >
+                            <option value='' selected  >Please select a type</option>
+                            <option value="MANAGER" >MANAGER</option>
+                            <option value="TL">TL</option>
+                             <option value="EMPLOYEE">EMPLOYEE</option>
+                          </select>
+                          </div>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                <button type="button" class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2" @click="saved">save</button>
+                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0" @click="open = false" ref="cancelButtonRef">Cancel</button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
         </div>
       </div>
     </Dialog>
   </TransitionRoot>
 </template>
 
-<script setup lang="ts">
-import { ref,defineEmits,defineProps } from 'vue'
-import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot,Switch,SwitchGroup, SwitchLabel,Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { XMarkIcon } from '@heroicons/vue/24/outline'
-import { ChevronDownIcon } from '@heroicons/vue/20/solid'
-// Define emits
-const emit=defineEmits(["saved"])
-const props=defineProps({
-    editPrefilledData:Object
-})
+<script setup>
+import { ref } from 'vue'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { CheckIcon } from '@heroicons/vue/24/outline'
 const open = ref(true)
+// Defining Props
+const props = defineProps({
+  editPrefilledData: {
+    type: Object,   
+  }
+});
+const emit=defineEmits(["saved"])
 const saved=()=>{
- emit("saved",props.editPrefilledData)
- open.value=false
+  open.value=false
+  emit("saved",props.editPrefilledData)
 }
-
 </script>
